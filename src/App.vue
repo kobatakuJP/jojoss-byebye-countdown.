779c3dccee796dfbe052b9f8dbef060292b6b5ea
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-03-28 16:30:47 +0900

    add countdownify

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,28 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+    <div class="content">
+      <Countdownify
+        msg="イベント"
+        :goalTime="Date.now() + 5000"
+        backgroundColor="rgba(255,0,0, 0.4)"
+        @its-time="timeup"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Countdownify from "vue-countdownify";
+import "vue-countdownify/dist/vue-countdownify.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Countdownify,
+  },
+};
+</script>
   }
 }
 </script>
