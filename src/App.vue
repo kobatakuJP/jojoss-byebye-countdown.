9c32993b37b55d65acbc44a9ca10cd519ee12e0a
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-03-28 18:18:14 +0900

    add movie

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="video-container">
+      <div class="video-wrap">
+        <video
+          src="../public/jojossmovie1_supershrink.mp4"
+          autoplay
+          loop
+          muted
+        >
+        </video>
+      </div>
+    </div>
     <div class="content">
       <Countdownify
-        msg="イベント"
-        :goalTime="Date.now() + 5000"
-        backgroundColor="rgba(255,0,0, 0.4)"
+        msg="ジョジョSS サ終"
+        :goalTime="[2021, 4, 13]"
+        backgroundColor="rgba(0,0,0,0)"
         @its-time="timeup"
       />
     </div>
@@ -25,17 +33,60 @@ export default {
   },
 };
 </script>
-  }
-}
-</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #20262e;
+}
+.content {
+  position: fixed;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(32, 38, 46, 0.8);
+}
+.video-container {
+  position: fixed;
+  z-index: 0;
+  margin: 0 auto;
+  width: 100%;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+
+.video-wrap {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 100vh;
+}
+
+video {
+  min-width: 100%;
+  min-height: 100vh;
+  position: absolute;
+}
+
+@media (aspect-ratio: 16/9), (min-aspect-ratio: 16/9) {
+  video {
+    width: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+
+@media (max-aspect-ratio: 16/9) {
+  video {
+    height: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 }
 </style>
