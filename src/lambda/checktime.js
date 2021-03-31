0423bed9ca615c396fc4538151931c373b3001df
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-03-31 18:55:49 +0900

    ikasama url

--- src/lambda/checktime.js
+++ src/lambda/checktime.js
@@ -1,10 +1,16 @@
+const TIME = 1618239600000; // Tue Apr 13 2021 00:00:00 GMT+0900
+const MARGIN_TIME = TIME - 60000; // 1分のマージンを取っておく
+const TRUE_URL = "";
+const FALSE_URL = "/ikasama_dameyo.jpeg";
+
 /** 時刻が予定を過ぎていればtrue, 過ぎていなければfalse */
 export function handler(event, context, callback) {
-  const TIME = 1618239600000; // Tue Apr 13 2021 00:00:00 GMT+0900
-  const MARGIN_TIME = TIME - 60000; // 1分のマージンを取っておく
   const isTimedUp = Date.now() > MARGIN_TIME ? true : false;
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({isTimedUp: isTimedUp})
+    body: JSON.stringify({
+      isTimedUp: isTimedUp,
+      url: isTimedUp ? TRUE_URL : FALSE_URL
+    })
   })
 }
