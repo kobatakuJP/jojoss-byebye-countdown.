946c354573be2a51e63ebf5a6ed95d2952008179
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-04-09 22:52:03 +0900

    add true url

--- src/lambda/checktime.js
+++ src/lambda/checktime.js
@@ -1,6 +1,6 @@
 const TIME = 1618239600000; // Tue Apr 13 2021 00:00:00 GMT+0900
 const MARGIN_TIME = TIME - 60000; // 1分のマージンを取っておく
-const TRUE_URL = "";
+const TRUE_URL = "/byebye-and-thanks-jojoss-20210413";
 const FALSE_URL = "/ikasama_dameyo.jpeg";
 
 /** 時刻が予定を過ぎていればtrue, 過ぎていなければfalse */
