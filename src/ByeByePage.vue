3d2ec0dc44ee18b31aed2200b5f00d67584c1861
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-04-10 15:48:25 +0900

    add end message

--- src/ByeByePage.vue
+++ src/ByeByePage.vue
@@ -13,13 +13,29 @@
         <SNSShareButtons class="sns-share-buttons" />
       </div>
     </div>
-    <div class="endrole">
-
+    <div class="filter">
+      <div class="endrole">
+        <h2>Thank you JOJO ss!</h2>
+        <h3>Thank you for...</h3>
+        <p class="bold">BANDAI NAMCO Entertainment Inc.</p>
+        <p class="bold">Drecom Co., Ltd.</p>
+        <p class="bold">集英社</p>
+        <p class="bold">LUCKY LAND COMMUNICATIONS</p>
+        <p class="bold">荒木飛呂彦大先生</p>
+        <p class="bold">and.... YOU!</p>
+        <br />
+        <br />
+        <br />
+        <p class="italy">つらいことがたくさんあったが・・・</p>
+        <p class="italy">でも楽しかったよ</p>
+        <p class="italy">みんながいたから この旅は楽しかった</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+// ロード→エンドロール→歴代トップ絵→
 import "vue-countdownify/dist/vue-countdownify.css";
 import SNSShareButtons from "./components/SNSShareButtons.vue";
 
@@ -63,9 +79,26 @@ body {
   width: 100%;
   height: 100%;
 }
+.filter {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+}
 .endrole {
   position: fixed;
   width: 100%;
   height: 100%;
+  text-align: center;
+  line-height: 300%;
+  color: white;
+}
+.bold {
+  font-weight: bold;
+}
+.italy {
+  font-style: italic;
+  font-family: 'Times New Roman','ヒラギノ明朝 Pro W6', 'Hiragino Mincho Pro', 'ＭＳ Ｐゴシック', serif;
 }
 </style>
